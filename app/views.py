c0549840d04679bbec94b340a3dbf36e6dc5c333
Deepakraja03 <12306deepakraja@gmail.com>
2023-04-01 21:40:53 +0530

    ALmost completed the project

--- app/views.py
+++ app/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,HttpResponse
+from .models import Student
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        dob = request.POST['dob']
+        gen = request.POST['gender']
+        cla = request.POST['className']
+        regn = request.POST['reg']
+        test = request.POST['test']
+        if float(test) > 100:
+            return HttpResponse("Test Score must be less than 100")
+        obj = Student()
+        obj.name = name
+        obj.email = email
+        obj.dob = dob
+        obj.gen = gen
+        obj.Stuclass = cla
+        obj.reg = regn
+        obj.test = test
+        obj.save()
+        return HttpResponse("<h1>Your entry have been saved</h1>")
     return render(request, 'index.html')
